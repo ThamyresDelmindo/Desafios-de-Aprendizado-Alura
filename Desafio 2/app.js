2be8797443bbcf4f9a224d4598f2b83c36ee60f9
ThamyresDelmindo <thamyres.delmindo22@gmail.com>
2025-03-01 21:29:34 -0300

    Add files via upload

---
+++ Desafio 2/app.js
@@ -0,0 +1,10 @@
+diaDaSemana = prompt('Qual o dia da semana?');
+if (diaDaSemana == 'Sábado') {
+    alert('Bom fim de semana');
+} else if (diaDaSemana == 'Domingo') {
+    alert('Bom fim de semana');
+} else {
+    alert ('Boa semana');
+        }
+        //NÃO CONSEGUI FAZER SOZINHA ESSA ATIVIDADE, EU ESTAVA COMPLICANDO TUDO E ERA SÓ ISSO,
+        //EU ESTAVA USANDO LET, MAS NEM PRECISAVA
